500da9e3947861a96ed4c27b79bd94c2c5ede08c
Tyler Crosse <tylerscottcrosse@gmail.com>
2016-02-23 13:31:13 -0500

    Initial commit, setting up project

---
+++ public/client.js
@@ -0,0 +1,8 @@
+$(function() {
+
+  //**** Socket Events ****//
+  socket.on('event' ,function (data) {
+    console.log('event');
+  });
+
+});
---
+++ server.py
@@ -0,0 +1,33 @@
+import json
+import os
+import time
+import socketio
+import eventlet
+from flask import Flask, Response, request, render_template
+
+sio = socketio.Server()
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Serve the client-side application."""
+    return render_template('index.html')
+
+@sio.on('connect')
+def connect(sid, environ):
+    print('connect ', sid)
+
+@sio.on('my message')
+def message(sid, data):
+    print('message ', data)
+
+@sio.on('disconnect')
+def disconnect(sid):
+    print('disconnect ', sid)
+
+if __name__ == '__main__':
+    # wrap Flask application with socketio's middleware
+    app = socketio.Middleware(sio, app)
+
+    # deploy as an eventlet WSGI server
+    eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
